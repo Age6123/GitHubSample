9d608813b7703dddc3b0aa653e8e658721c56151
YoneokaTakeshi <yoneoka@supersoftware.co.jp>
2016-06-28 17:12:30 +0900

    一回目HTTPリクエスト非同期化

--- GitHubSample/AppDelegate.m
+++ GitHubSample/AppDelegate.m
@@ -59,44 +59,107 @@
     NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:[zipSearchApi searchZipNumberToAddress]]];
     
     // サーバーとの通信を行う
-    NSData *json = [NSURLConnection sendSynchronousRequest:request
-                                         returningResponse:nil
-                                                     error:nil];
-    // JSONをパース
-    NSArray *array = [NSJSONSerialization JSONObjectWithData:json
-                                                     options:NSJSONReadingAllowFragments
-                                                       error:nil];
+    [NSURLConnection sendAsynchronousRequest:request
+                                       queue:[[NSOperationQueue alloc] init]
+                           completionHandler:^(NSURLResponse *response, NSData *json, NSError *error) {
+                              
+                               if (error) {
+                                   // エラー処理を行う。
+                                   NSLog(@"Request Error:%@", error);
+//                                   if (error.code == -1003) {
+//                                       NSLog(@"not found hostname. targetURL=%@", [zipSearchApi searchZipNumberToAddress]);
+//                                   } else if (-1019) {
+//                                       NSLog(@"auth error. reason=%@", error);
+//                                   } else {
+//                                       NSLog(@"unknown error occurred. reason = %@", error);
+//                                   }
+                                   
+                               } else {
+                                   NSLog(@"Request OK");
+                                   
+                                   NSInteger httpStatusCode = ((NSHTTPURLResponse *)response).statusCode;
+                                   if (httpStatusCode == 404) {
+                                       NSLog(@"404 NOT FOUND ERROR. targetURL=%@", [zipSearchApi searchZipNumberToAddress]);
+                                   } else {
+                                       NSLog(@"success request!!");
+                                       NSLog(@"statusCode = %ld", ((NSHTTPURLResponse *)response).statusCode);
+                                       NSLog(@"responseText = %@", [[NSString alloc] initWithData:json encoding:NSUTF8StringEncoding]);
+                                       
+                                       // JSONをパース
+                                       NSArray *array = [NSJSONSerialization JSONObjectWithData:json
+                                                                                        options:NSJSONReadingAllowFragments
+                                                                                          error:nil];
+                                       // 郵便番号検索APIに取得した情報を格納
+                                       zipSearchApi.state     = [array valueForKeyPath:@"state"];
+                                       zipSearchApi.stateName = [array valueForKeyPath:@"stateName"];
+                                       zipSearchApi.city      = [array valueForKeyPath:@"city"];
+                                       zipSearchApi.street    = [array valueForKeyPath:@"street"];
+                                       
+                                       
+                                       // メインスレッドでの処理
+                                       dispatch_async(dispatch_get_main_queue(), ^{
+                                           // NSURLからNSURLRequestを作る
+                                           NSURLRequest *request2 = [NSURLRequest requestWithURL:[NSURL URLWithString:[zipSearchApi searchAddressToZipNumber]]];
+                                           
+                                           // サーバーとの通信を行う
+                                           NSError *error;
+                                           NSData *json2 = [NSURLConnection sendSynchronousRequest:request2
+                                                                                 returningResponse:nil
+                                                                                             error:&error];
+                                           if(error){
+                                               NSLog(@"%@", error);
+                                           }
+                                           
+                                           // JSONをパース
+                                           NSArray *array2 = [NSJSONSerialization JSONObjectWithData:json2
+                                                                                             options:NSJSONReadingAllowFragments
+                                                                                               error:nil];
+                                           // 取得した郵便番号を表示
+                                           NSLog(@"zipcode:%@", [array2 valueForKey:@"zipcode"]);
+                                       });
+                                   }
+                               }
+                           }];
     
-    // 郵便番号検索APIに取得した情報を格納
-    zipSearchApi.state     = [array valueForKeyPath:@"state"];
-    zipSearchApi.stateName = [array valueForKeyPath:@"stateName"];
-    zipSearchApi.city      = [array valueForKeyPath:@"city"];
-    zipSearchApi.street    = [array valueForKeyPath:@"street"];
     
-    NSLog(@"state:%@",     zipSearchApi.state);
-    NSLog(@"stateName:%@", zipSearchApi.stateName);
-    NSLog(@"city:%@",      zipSearchApi.city);
-    NSLog(@"street:%@",    zipSearchApi.street);
-    
-    
-    // NSURLからNSURLRequestを作る
-    NSURLRequest *request2 = [NSURLRequest requestWithURL:[NSURL URLWithString:[zipSearchApi searchAddressToZipNumber]]];
-    
-    // サーバーとの通信を行う
-    NSError *error;
-    NSData *json2 = [NSURLConnection sendSynchronousRequest:request2
-                                         returningResponse:nil
-                                                     error:&error];
-    if( error != nil ){
-        NSLog(@"%@", error);
-    }
-    
-    // JSONをパース
-    NSArray *array2 = [NSJSONSerialization JSONObjectWithData:json2
-                                                     options:NSJSONReadingAllowFragments
-                                                       error:nil];
-    // 取得した郵便番号を表示
-    NSLog(@"zipcode:%@", [array2 valueForKey:@"zipcode"]);
+//    NSData *json = [NSURLConnection sendSynchronousRequest:request
+//                                         returningResponse:nil
+//                                                     error:nil];
+//    // JSONをパース
+//    NSArray *array = [NSJSONSerialization JSONObjectWithData:json
+//                                                     options:NSJSONReadingAllowFragments
+//                                                       error:nil];
+//    
+//    // 郵便番号検索APIに取得した情報を格納
+//    zipSearchApi.state     = [array valueForKeyPath:@"state"];
+//    zipSearchApi.stateName = [array valueForKeyPath:@"stateName"];
+//    zipSearchApi.city      = [array valueForKeyPath:@"city"];
+//    zipSearchApi.street    = [array valueForKeyPath:@"street"];
+//    
+//    NSLog(@"state:%@",     zipSearchApi.state);
+//    NSLog(@"stateName:%@", zipSearchApi.stateName);
+//    NSLog(@"city:%@",      zipSearchApi.city);
+//    NSLog(@"street:%@",    zipSearchApi.street);
+//    
+//    
+//    // NSURLからNSURLRequestを作る
+//    NSURLRequest *request2 = [NSURLRequest requestWithURL:[NSURL URLWithString:[zipSearchApi searchAddressToZipNumber]]];
+//    
+//    // サーバーとの通信を行う
+//    NSError *error;
+//    NSData *json2 = [NSURLConnection sendSynchronousRequest:request2
+//                                         returningResponse:nil
+//                                                     error:&error];
+//    if( error != nil ){
+//        NSLog(@"%@", error);
+//    }
+//    
+//    // JSONをパース
+//    NSArray *array2 = [NSJSONSerialization JSONObjectWithData:json2
+//                                                     options:NSJSONReadingAllowFragments
+//                                                       error:nil];
+//    // 取得した郵便番号を表示
+//    NSLog(@"zipcode:%@", [array2 valueForKey:@"zipcode"]);
 }
 
 - (void)requestTokenTicket:(OAServiceTicket *)ticket
